82e7d6cc321371ff566fc0cb477443190421caf2
Bharath Ram Sainadh Malisetti <sainadhmalisetti666@gmail.com>
2026-05-11 18:02:28 +0530

    after solving merge conflict

--- main.c
+++ main.c
@@ -6,10 +6,7 @@ printf("after the commite editing this");
 printf("for git diff checking");
 printf("for staged git diff purpose only");
 printf("for first new branch created purpose and added this after the master commit also ");
-<<<<<<< HEAD
 printf("thisis for rebase check in main");
-=======
 printf("it is created for testing and chnaging same line purpose");
 printf("it is created for rebase what it will do purpose");
->>>>>>> 20d167f (thisis for testing and understanding the rebase for first time)
 }

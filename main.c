5c9b36a861a7fd13fff79eb22db801cd04d99fe6
Bharath Ram Sainadh Malisetti <sainadhmalisetti666@gmail.com>
2026-05-11 11:26:35 +0530

    for staged git diff purpose only

--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 int main(){
 printf("run automatically using jenkins");
 printf("after the commite editing this");
-printtf("for git diff checking");
+printf("for git diff checking");
+printf("for staged git diff purpose only");
 }
